932a05fc13e598987d7925d60d9f9da5bc329296
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2022-07-08 09:36:35 -0300

    Update 01-databases.sql

--- mysql-few-databases/docker/provision/mysql/init/01-databases.sql
+++ mysql-few-databases/docker/provision/mysql/init/01-databases.sql
@@ -1,7 +1,7 @@
-# create databases
+-- create databases
 CREATE DATABASE IF NOT EXISTS `primary`;
 CREATE DATABASE IF NOT EXISTS `secondary`;
 
-# create root user and grant rights
+-- create root user and grant rights
 CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
-GRANT ALL ON *.* TO 'root'@'%';
+GRANT ALL ON *.* TO 'root'@'localhost';
